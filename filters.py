35bd099186c25523c9e48349807b2cf24777c1c1
Ujjwal Gupta <ujjwal.gupta.16csc@bml.edu.in>
2019-08-13 23:18:54 +0530

    Updated Kernal - Cartoon function

--- filters.py
+++ filters.py
@@ -17,30 +17,26 @@ def edge(img):
        C=5)
     return img_edge
 
-def cartoon(img_rgb):
+def Cartoonize(img_rgb):
+    num_down=1
     num_bilateral = 5 # number of bilateral filtering steps
-    
+
+    # downsample image using Gaussian pyramid
+    img_color = img_rgb
+    for _ in range(num_down):
+        img_color = cv2.pyrDown(img_color)
     for _ in range(num_bilateral):
-        img_color = cv2.bilateralFilter(img_rgb, d=9, sigmaColor=9, sigmaSpace=7)
-    
-    
-    img_gray = cv2.cvtColor(img_color, cv2.COLOR_RGB2GRAY)
+        img_color = cv2.bilateralFilter(img_color, d=9, sigmaColor=9, sigmaSpace=7)
+    for _ in range(num_down):
+        img_color = cv2.pyrUp(img_color)
+
+    img_gray = cv2.cvtColor(img_rgb, cv2.COLOR_RGB2GRAY)
     img_blur = cv2.medianBlur(img_gray, 7)
-    
-    #Use adaptive thresholding to create an edge mask
-    # detect and enhance edges
-    img_edge = cv2.adaptiveThreshold(img_blur, 255,
-       cv2.ADAPTIVE_THRESH_MEAN_C,
-       cv2.THRESH_BINARY,
-       blockSize=11,
-       C=6)
-    
+    img_edge = cv2.adaptiveThreshold(img_blur, 255, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY, blockSize=9, C=4)
     img_edge = cv2.cvtColor(img_edge, cv2.COLOR_GRAY2RGB)
-    
-    # Combine color image with edge mask & display picture
-    # convert back to color, bit-AND with color image
+    dim = (img_edge.shape[1],img_edge.shape[0])
+    img_color = cv2.resize(img_color,dim)
     img_cartoon = cv2.bitwise_and(img_color, img_edge)
-    
     return img_cartoon
 
 def sharp(img):
